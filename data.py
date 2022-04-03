b504371ca2c600dc9c835002221cf5c328831ff1
venki05 <69805785+venki05@users.noreply.github.com>
2022-04-03 08:22:07 +0530

    Add files via upload

---
+++ data.py
@@ -0,0 +1,7 @@
+#Railway Stations
+places=["chennai","coimbatore","madurai"]
+
+#trains
+trains={"chennai":{"coimbatore":[500,500],"madurai":[500,500]},"madurai":{"chennai":[500,500],"coimbatore":[500,500]},"coimbatore":{"chennai":[500,500],"madurai":[500,500]}}
+
+
