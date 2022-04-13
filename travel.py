b6c798772e1081d0a865364296ade9c2176c1c86
venki05 <69805785+venki05@users.noreply.github.com>
2022-04-13 18:19:16 +0530

    Update travel.py

--- travel.py
+++ travel.py
@@ -4,34 +4,22 @@ des='*'
 space=' '
 log.login_credentials()
 while True:
-    print()
-    departure_place=input("enter the departure place: ").lower()
+    departure_place=input("\n enter the departure place: ").lower()
     if departure_place in d.places:
-        print()
-        destination_place=input("enter the destination place: ").lower()
+        destination_place=input("\n enter the destination place: ").lower()
         if destination_place in d.places:
-            print()
-            print("Train name: "+destination_place+" Express")
-            print()
-            print("Total number of tickets:",d.trains[departure_place][destination_place][0])
-            print()
-            print("Number of available tickets:",d.trains[departure_place][destination_place][1])
-            print()
-            book_ticket=input("Do you want book tickets for your journey in this train. Enter Y for yes and N for no: ")
+            print("\n Train name: "+destination_place+" Express")
+            print("\n Total number of tickets:",d.trains[departure_place][destination_place][0])
+            print("\n Number of available tickets:",d.trains[departure_place][destination_place][1])
+            book_ticket=input("\n Do you want book tickets for your journey in this train. Enter Y for yes and N for no: ")
             if book_ticket=='y':
-                print()
-                num_tickets=int(input("enter the number of tickets to be booked: "))
+                num_tickets=int(input("\n enter the number of tickets to be booked: "))
                 if num_tickets<=d.trains[departure_place][destination_place][1]:
                     for i in range(1,num_tickets+1):
-                        print()
-                        name=input("enter the name of the passanger "+str(i)+":").upper()
-                        print()
-                        DOB=input("enter the date of birth of the passenger "+str(i)+" in the format dd-mm-yyyy: ")
-                        print()
-                        verify_num=input("enter your adhaar number or pan number: ")
-                        print()
-                        print("successfully booked ticket for passenger"+str(i)+". Below is the E-Ticket.")
-                        print()
+                        name=input("\n enter the name of the passanger "+str(i)+":").upper()
+                        DOB=input("\n enter the date of birth of the passenger "+str(i)+" in the format dd-mm-yyyy: ")
+                        verify_num=input("\n enter your adhaar number or pan number: ")
+                        print("\n successfully booked ticket for passenger"+str(i)+". Below is the E-Ticket.")
                         print(des*40)
                         print(space*5,"Name:",name)
                         print(space*5,"DOB:",DOB)
@@ -40,29 +28,28 @@ while True:
                         print()
                         print(des*40)
                         d.trains[departure_place][destination_place][1]=d.trains[departure_place][destination_place][1]-1
-                        opt=input("Do you want to book tickets for your journey once again? If yes enter Y ").lower()
-                        if opt=='y':
-                            continue
-                        else:
-                            print("Thanks for booking your tickets through us /n Have a great journey!!!")
-                            break
+                    opt=input("Do you want to book tickets for your journey once again? If yes enter Y ").lower()
+                    if opt=='y':
+                        continue
+                    else:
+                        print("Thanks for booking your tickets through us /n Have a great journey!!!")
+                        break
                 else:
                     print("All the seats are full at present")
                     break
         else:
             print("There are no direct trains at present")
-            opt=input("Do you want to book tickets for your journey once again? If yes enter Y ").lower()
+            opt=input("Do you want to book tickets for your journey? If yes enter Y ").lower()
             if opt=='y':
                 continue
             else:
-                print("Thanks for booking your tickets through us /n Have a great journey!!!")
+                print("Thanks for booking your tickets through us \nHave a great journey!!!")
                 break
     else:
         print("There are no trains departing from this place")
-        opt=input("Do you want to book tickets for your journey once again? If yes enter Y ").lower()
+        opt=input("Do you want to book tickets for your journey ? If yes enter Y ").lower()
         if opt=='y':
             continue
         else:
-            print("Thanks for booking your tickets through us /n Have a great journey!!!")
+            print("Thanks for booking your tickets through us \nHave a great journey!!!")
             break
-    
